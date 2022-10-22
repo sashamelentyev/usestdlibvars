5959729550688e5f5782053dd70a47c5d893d307
Sasha Melentyev <sasha@melentyev.io>
2022-10-22 17:58:21 +0300

    fix issue 85

--- pkg/analyzer/internal/mapping/mapping.go
+++ pkg/analyzer/internal/mapping/mapping.go
@@ -7,7 +7,6 @@ import (
 	"go/constant"
 	"net/http"
 	"net/rpc"
-	"os"
 	"strconv"
 	"time"
 )
@@ -164,9 +163,7 @@ var TimeLayout = map[string]string{
 	time.StampNano:   "time.StampNano",
 }
 
-var OSDevNull = map[string]string{
-	os.DevNull: "os.DevNull",
-}
+var OSDevNull = map[string]string{}
 
 var SQLIsolationLevel = map[string]string{
 	// sql.LevelDefault.String():         "sql.LevelDefault.String()",
